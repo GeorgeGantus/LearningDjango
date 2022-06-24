0d01b2666a7b28088ac4c367de36801862b6fa44
George Gantus <georgegantus@usp.br>
2022-06-24 14:56:05 -0300

    fix: only show published recipes

--- recipes/views.py
+++ recipes/views.py
@@ -4,14 +4,14 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html',
                   context={'recipes': recipes})
 
 
 def category(request, category_id):
     recipes_by_category = Recipe.objects.filter(
-        category__id=category_id).order_by('-id')
+        category__id=category_id, is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html',
                   context={'recipes': recipes_by_category})
 
