8d9037a91f7579fbaf7597fdfa51fb11bdbc2a2b
George Gantus <georgegantus@usp.br>
2022-06-27 20:41:13 -0300

    test: testing search

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -74,3 +74,15 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:recipe', kwargs={
                                    'id': recipe.id}))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_view_function_is_correct(self):
+        view = resolve(reverse('recipes:search'))
+        self.assertEqual(view.func, views.search)
+
+    def test_recipe_search_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:search') + '?q=search')
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_view_raises_404_if_no_search(self):
+        response = self.client.get(reverse('recipes:search'))
+        self.assertEqual(response.status_code, 404)
