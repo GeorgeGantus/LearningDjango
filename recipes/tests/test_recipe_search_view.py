23ebd64a9e002d1ac84fc7055238a06a015ba99f
George Gantus <georgegantus@usp.br>
2022-06-28 10:47:46 -0300

    test: search view recipes test

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -23,3 +23,44 @@ class RecipeSearchViewTest(RecipeTestBase):
         response = self.client.get(url)
         self.assertIn('Search for "&lt;notarealtag&gt;"',
                       response.content.decode('utf-8'))
+
+    def test_recipe_search_view_can_find_recipe_by_title(self):
+        title1 = 'This is recipe 1'
+        title2 = 'This is recipe 2'
+
+        recipe1 = self.make_recipe(
+            slug='one', title=title1, user_data={'username': 'one'}
+        )
+        recipe2 = self.make_recipe(
+            slug='two', title=title2, user_data={'username': 'two'}
+        )
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?q={title1}')
+        response2 = self.client.get(f'{search_url}?q={title2}')
+        response_both = self.client.get(f'{search_url}?q=this')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
+
+    def test_recipe_search_view_only_show_published_recipes(self):
+        title1 = 'This is a published recipe'
+        title2 = 'This is a not published recipe'
+
+        recipe1 = self.make_recipe(
+            slug='published', title=title1, user_data={'username': 'one'}
+        )
+        recipe2 = self.make_recipe(
+            slug='not-published', is_published=False,
+            title=title2, user_data={'username': 'two'}
+        )
+
+        response = self.client.get(reverse('recipes:search')+'?q=published')
+        self.assertIn(recipe1, response.context['recipes'])
+        self.assertNotIn(recipe2, response.context['recipes'])
