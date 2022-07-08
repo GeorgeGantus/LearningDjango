b4c55eafd262b56b9759760c934cae4d9b895ac0
George Gantus <georgegantus@usp.br>
2022-07-08 20:29:26 -0300

    feat: improving django admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -14,6 +14,16 @@ admin.site.register(Category, CategoryAdmin)
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'title', 'is_published', 'author')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'description', 'preparation_steps', 'slug')
+    list_filter = ('category', 'author', 'is_published',
+                   'preparation_steps_is_html')
+    list_per_page = 10
+    list_editable = ('is_published')
+    ordering = '-id'
+    prepopulated_fields = {
+        'slug': ('title')
+    }
 
 # End - Examples
