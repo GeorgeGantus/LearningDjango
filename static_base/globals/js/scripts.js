3d70e7219ffaa8878dae9d90bc46754cd302b056
George Gantus <georgegantus@usp.br>
2022-07-17 14:58:15 -0300

    feat: creating side menu

--- static_base/globals/js/scripts.js
+++ static_base/globals/js/scripts.js
@@ -1,4 +1,4 @@
-function my_scope() {
+(() => {
     const forms = document.querySelectorAll('.delete-form')
     for (const form of forms) {
         form.addEventListener('submit', function (e) {
@@ -9,5 +9,50 @@ function my_scope() {
             }
         })
     }
-}
-my_scope();
+})();
+
+(() => {
+    const buttonOpenMenu = document.querySelector('.menu-open-button')
+    const buttonCloseMenu = document.querySelector('.menu-close-button')
+    const menuContainer = document.querySelector('.menu-container')
+
+    const menuHiddenClass = 'menu-hidden'
+    const buttonOpenMenuHiddenClass = 'menu-open-button-hidden'
+
+    const closeMenu = () => {
+        menuContainer.classList.add(menuHiddenClass)
+        buttonOpenMenu.classList.remove(buttonOpenMenuHiddenClass)
+    }
+
+    const showMenu = () => {
+        menuContainer.classList.remove(menuHiddenClass)
+        buttonOpenMenu.classList.add(buttonOpenMenuHiddenClass)
+    }
+
+    if (buttonCloseMenu) {
+        buttonCloseMenu.removeEventListener('click', closeMenu)
+        buttonCloseMenu.addEventListener('click', closeMenu)
+    }
+    if (buttonOpenMenu) {
+        buttonOpenMenu.removeEventListener('click', showMenu)
+        buttonOpenMenu.addEventListener('click', showMenu)
+    }
+
+    closeMenu()
+
+})();
+
+(() => {
+    const logoutLinks = document.querySelectorAll('.logout-link')
+    const logoutForm = document.querySelector('.logout-form')
+
+    const logout = (e) => {
+        e.preventDefault()
+        logoutForm.submit()
+    }
+
+    for (const link of logoutLinks) {
+        link.removeEventListener('click', logout)
+        link.addEventListener('click', logout)
+    }
+})();
